ce2aafb7252a91824568c1687114237bcf74479b
PacemqueDones <anderson.vwd@gmail.com>
2026-03-17 14:43:20 -0300

    satup-correction

--- setup.py
+++ setup.py
@@ -2,13 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="fair-topsis",
-    version="0.1",
+    version="0.1.0",
     packages=find_packages(),
     install_requires=[
-        "numpy",
-        "scikit-learn",
-        "matplotlib"
+        "numpy>=1.20"
     ],
+    include_package_data=True,
     description="Framework para seleção de modelos fair utilizando TOPSIS",
     author="Anderson Lucas de Paula Costa",
 )
